c211522b959744b3ead451592f8c1bb13314d5d9
marcanta <aime.marcant@hotmail.fr>
2021-05-11 17:38:58 +0200

    add category

--- src/containers/Details.js
+++ src/containers/Details.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ImageService from '../API/ImageService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faDownload } from '@fortawesome/free-solid-svg-icons';
+
 import './Details.css';
 
 export default class Details extends React.Component {
@@ -21,16 +24,38 @@ export default class Details extends React.Component {
         if (this.state.loading) {
             return <h1>Loading...</h1>;
         } else {
-            const { name, description, imageUrl } = this.state.image;
+            const { name, description, date, category, objects, nbPersons, imageUrl } = this.state.image;
             return (
                 <div>
-                    <div class="apercu" style={{ backgroundImage: `url(${imageUrl})`}}>
+                    <div class="apercu d-flex flex-row-reverse" style={{ backgroundImage: `url(${imageUrl})` }}>
+                        <a class="my-link-white" style={{ paddingRight: "2rem" }} href={imageUrl} download>
+                            <FontAwesomeIcon  icon={faDownload} style={{fontSize: "2rem"}}/>
+                        </a>
                     </div>
-                    <h1>{name}</h1>
-                    <h2>For {this.props.imageId}</h2>
-                    <p>{description}</p>
+                    <div >
+                        <div class="d-flex flex-row justify-content-center">
+                            <h1>{name}</h1>
+                        </div>
+                        <div class="d-flex flex-row justify-content-center">
+                            <h3 class="me-4">Date: {new Date(date).toISOString().split('T')[0]}</h3>
+                            <h3>Category: {category}</h3>
+                        </div>
+                        <h3>{description}</h3>  
+                    </div>
                 </div>
             );
         }
     }
 }
+
+
+{/* <div class="dropdown">
+                            <a class="my-link-white dropdown-toggle" style={{paddingRight: "2rem"}}>
+                                <FontAwesomeIcon  icon={faEllipsisH} style={{fontSize: "5rem"}}/>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </div> */}
--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -8,6 +8,7 @@ export default class Gallery extends React.Component {
     this.state = {
       images: [],
       categories: [],
+      selectedCategory: "."
     };
   }
   
@@ -19,10 +20,22 @@ export default class Gallery extends React.Component {
   }
   
   render() {
+    const { images, categories, selectedCategory } = this.state;
+    console.log(selectedCategory);
     return (
       <div class="container">
+        <div class="btn-group mb-4" role="group" aria-label="Basic radio toggle button group">
+          <input type="radio" class="btn-check" name="btnradio" id="btnradio1" onChange={() => this.setState({ selectedCategory: "." })} value="." autocomplete="off" checked={selectedCategory === '.'}/>
+          <label class="btn btn-outline-primary" for="btnradio1">All</label>
+          {categories.map(category => (
+            <>
+              <input type="radio" class="btn-check" name="btnradio" onChange={() => this.setState({ selectedCategory: category })} id={category} value={category} autocomplete="off"/>
+             <label class="btn btn-outline-primary" for={category}>{category}</label>
+            </>
+          ))}
+        </div>
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
-          {this.state.images.map(photo => <Photo key={Math.random()*1000} {...photo} />)}
+          {images.filter(image => image.published && image.category.match(selectedCategory)).map(photo => <Photo key={Math.random()*1000} {...photo} />)}
         </div>
       </div>
     );
